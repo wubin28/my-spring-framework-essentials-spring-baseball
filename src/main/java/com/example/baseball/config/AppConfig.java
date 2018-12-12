b6e0ddca15a8e66333a0aee9d5a416ac34186722
Ben WU <wubin28@gmail.com>
2018-12-12 21:59:27 +0800

    use qualifier annotation for autowiring beans of the same type

--- src/main/java/com/example/baseball/config/AppConfig.java
+++ src/main/java/com/example/baseball/config/AppConfig.java
@@ -4,24 +4,28 @@ import com.example.baseball.domain.BaseballGame;
 import com.example.baseball.domain.Game;
 import com.example.baseball.domain.Team;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-
-import java.util.List;
 
 @Configuration
 @ComponentScan(basePackages = "com.example.baseball")
 public class AppConfig {
 
     @Autowired
-    private List<Team> teams;
+    @Qualifier("redSox")
+    private Team home;
+
+    @Autowired
+    @Qualifier("cubs")
+    private Team away;
 
     @Bean
     @Scope("prototype")
     public Game game() {
-        BaseballGame baseballGame = new BaseballGame(teams.get(0), teams.get(1));
+        BaseballGame baseballGame = new BaseballGame(home, away);
         return baseballGame;
     }
 }
