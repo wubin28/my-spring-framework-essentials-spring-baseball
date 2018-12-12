01dd34090adf6bd3b37033ab64a77e4fecbbf21e
Ben WU <wubin28@gmail.com>
2018-12-12 20:59:33 +0800

    make test should_have_bean_definition_names green

--- src/test/java/com/example/baseball/domain/BaseballGameTests.java
+++ src/test/java/com/example/baseball/domain/BaseballGameTests.java
@@ -9,6 +9,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -36,5 +39,21 @@ public class BaseballGameTests {
 	public void should_get_a_bean_from_application_context() {
 		Team royals = context.getBean("royals", Team.class);
 		assertEquals("Kansas City Royals", royals.getName());
+	}
+
+	@Test
+	public void should_have_bean_definition_names() {
+		assertEquals("org.springframework.context.annotation.internalConfigurationAnnotationProcessor\n" +
+				"org.springframework.context.annotation.internalAutowiredAnnotationProcessor\n" +
+				"org.springframework.context.annotation.internalCommonAnnotationProcessor\n" +
+				"org.springframework.context.event.internalEventListenerProcessor\n" +
+				"org.springframework.context.event.internalEventListenerFactory\n" +
+				"appConfig\n" +
+				"org.springframework.boot.test.mock.mockito.MockitoPostProcessor$SpyPostProcessor\n" +
+				"org.springframework.boot.test.mock.mockito.MockitoPostProcessor\n" +
+				"cubs\n" +
+				"redSox\n" +
+				"royals\n" +
+				"game", Arrays.stream(context.getBeanDefinitionNames()).map(name -> name.toString()).collect(Collectors.joining("\n")));
 	}
 }
