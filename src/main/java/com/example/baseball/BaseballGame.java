ff0d77bf5089ba0b14c09aa8db5b79ed64003663
Ben WU <wubin28@gmail.com>
2018-12-12 18:22:33 +0800

    change the app name

--- src/main/java/com/example/baseball/BaseballGame.java
+++ src/main/java/com/example/baseball/BaseballGame.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseballApplication {
+public class BaseballGame {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseballApplication.class, args);
+		SpringApplication.run(BaseballGame.class, args);
 	}
 }
--- src/test/java/com/example/baseball/BaseballGameTests.java
+++ src/test/java/com/example/baseball/BaseballGameTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BaseballApplicationTests {
+public class BaseballGameTests {
 
 	@Test
 	public void contextLoads() {
