2251f83c4c4f5436b08fa74c1ad5590aec1537e0
Ben WU <wubin28@gmail.com>
2018-12-12 20:49:57 +0800

    make test should_get_a_bean_from_application_context green

--- src/test/java/com/example/baseball/domain/BaseballGameTests.java
+++ src/test/java/com/example/baseball/domain/BaseballGameTests.java
@@ -3,10 +3,13 @@ package com.example.baseball.domain;
 import com.example.baseball.config.AppConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -15,6 +18,9 @@ public class BaseballGameTests {
 
 	@Autowired
 	private Game game;
+
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
 	public void should_have_a_winning_team() {
@@ -26,4 +32,9 @@ public class BaseballGameTests {
 		assertTrue(result.contains(home) || result.contains(away));
 	}
 
+	@Test
+	public void should_get_a_bean_from_application_context() {
+		Team royals = context.getBean("royals", Team.class);
+		assertEquals("Kansas City Royals", royals.getName());
+	}
 }
