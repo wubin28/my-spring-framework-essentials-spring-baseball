066e8bf1081dd0b07b455656b65712fdfc711244
Ben WU <wubin28@gmail.com>
2018-12-13 18:15:15 +0800

    pre destroy bean method got called for a singleton bean after close the application context

--- src/main/java/com/example/baseball/config/AppConfig.java
+++ src/main/java/com/example/baseball/config/AppConfig.java
@@ -27,7 +27,6 @@ public class AppConfig {
     }
 
     @Bean
-    @Scope("prototype")
     public Game game() {
         BaseballGame baseballGame = new BaseballGame(teams.get(0), teams.get(1));
         baseballGame.setDataSource(dataSource);
