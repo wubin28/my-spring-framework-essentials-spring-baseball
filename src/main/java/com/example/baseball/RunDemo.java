e477cd0c1d8c8ac6192a3d6b4f0d2d80eae255ed
Ben WU <wubin28@gmail.com>
2018-12-13 17:05:01 +0800

    use factory method attribute in the xml for an abstract class

--- src/main/java/com/example/baseball/RunDemo.java
+++ src/main/java/com/example/baseball/RunDemo.java
@@ -1,24 +1,16 @@
 package com.example.baseball;
 
-import com.example.baseball.config.AppConfig;
-import com.example.baseball.domain.Game;
-import com.example.baseball.domain.Team;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.NumberFormat;
 
 public class RunDemo {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Team royals = context.getBean("royals", Team.class);
-
-        Game game1 = context.getBean("game", Game.class);
-        System.out.println(game1);
-
-        Game game2 = context.getBean("game", Game.class);
-        game2.setAwayTeam(royals);
-        System.out.println(game2);
-
-        System.out.println(game1);
+        NumberFormat numberFormat = context.getBean(NumberFormat.class);
+        double amount = 12345678.9012345;
+        System.out.println(numberFormat.format(amount));
     }
 }
