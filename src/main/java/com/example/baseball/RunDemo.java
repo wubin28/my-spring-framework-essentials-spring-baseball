ee9c70e2dde5686d376ea34f20509406cdbad7e6
Ben WU <wubin28@gmail.com>
2018-12-13 08:28:45 +0800

    add a main() method

---
+++ src/main/java/com/example/baseball/RunDemo.java
@@ -0,0 +1,14 @@
+package com.example.baseball;
+
+import com.example.baseball.config.AppConfig;
+import com.example.baseball.domain.Game;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class RunDemo {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Game game = context.getBean("game", Game.class);
+        System.out.println(game.playGame());
+    }
+}
